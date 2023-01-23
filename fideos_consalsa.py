e7f25194f14a3a9469ad1fcc885aeb1aab86d12c
Vee-v <46638059+Vee-v@users.noreply.github.com>
2023-01-23 01:43:33 -0300

    bugfix with dontObserve and multiple planets in star system

--- fideos_consalsa.py
+++ fideos_consalsa.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import warnings
 # import json
 from astropy import units as u
 from astropy.coordinates import SkyCoord , Angle, get_sun, EarthLocation, AltAz, get_moon
@@ -77,87 +78,96 @@ for i in dontObserve:
     print(f'\t{i}')
     # Reads the target names that won't be observed.
 contador = 0
-for day in range(days_range):
-    if weekday == 0:  # Monday
-        if not(day % 7 == 0 or (day + 5) % 7 == 0):
-            continue  # In this version this statement skips days that are not M,W.
-    elif weekday == 2:  # Wednesday
-        if not(day % 7 == 0 or (day + 2) % 7 == 0):
-            continue  # In this version this statement skips days that are not W,M.
-    times = Time(datetime(int(date[0]), int(date[1]), int(date[2]), 23, 59, 0)) + day*u.day + np.linspace(-6, 9, 901)* u.hour
-    # Ahora se busca el momento donde el sol cruza los -18° de altitud desde la posicion en La Silla
-    
-    sunPos = get_sun(times)
-    aaFrame = AltAz(obstime=times, location=laSilla)
-    aa_sunPos = sunPos.transform_to(aaFrame) 
-    begin = np.argmax(aa_sunPos.alt.value < -12)
-    end = np.argmax(np.flip(aa_sunPos.alt).value < -18)
-    twiBegin = times[begin]
-    twiEnd = np.flip(times)[end]
-    # Ahora que tenemos la hora de inicio y de termino del twilight, tenemos el tiempo para calcular efemerides de luna y targets.
-    obsTimeDelta = twiEnd + .5*u.hour - (twiBegin - .5*u.hour)
-    obsTimeDelta = TimeDelta((obsTimeDelta.value * 24 // 1) / 24)
-    steps = int(np.round(obsTimeDelta.sec,0) / 60)
-    obsDateTime = twiBegin - .5*u.hour + obsTimeDelta * np.linspace(0, 1, steps+1)
-    with open('nightStartTime.txt', 'a') as f:
-        f.write(str(day)+'|'+
-                str(obsDateTime[0].datetime.year)+'|'+
-                str(obsDateTime[0].datetime.month)+'|'+
-                str(obsDateTime[0].datetime.day)+'|'+
-                str(obsDateTime[0].datetime.hour)+'|'+
-                str(obsDateTime[0].datetime.minute)+'\n'
-               )
-    # Marco AltAz de La Silla
-    aaFrame = AltAz(obstime=obsDateTime, location=laSilla)
-    # Efemerides Luna
-    moonPos = get_moon(obsDateTime, laSilla)
-    aa_moonPos = moonPos.transform_to(aaFrame)  # TODO: output the Moon distance in the sky between targets and the moon.
-    # Indices de muestreo para la altura del target cada una hora
-    samplingIndexes = np.linspace(30, steps-30, int(steps/60), dtype=int)
-    sampledOutput   = np.zeros([len(TOI_df)+len(CTOI_df), int(steps/60)])
-    y = 0
-    for target in TOI_df.iloc:
-        targetPos = SkyCoord(ra=target['RA'], dec=target['Dec'], distance=target['Stellar Distance (pc)'] ,
-                             unit=(u.hourangle, u.deg, u.pc), obstime=obsDateTime)
-        with erfa_astrom.set(ErfaAstromInterpolator(int(steps/60) * u.hour)): 
-            temp = targetPos.transform_to(aaFrame)
-            if np.array([temp.alt.value>=50]).sum() >= 60 and target['SG2'] <= 3 and not(target['TIC ID'] in dontObserve):
-                # Si el target esta sobre 50 grados por 1 hora o mas, se muestrea cada 1 hora.
-                x = 0
-                for i in samplingIndexes:
-                    # Si no esta sobre 50 grados por 1 hora minimo, no se rellena.
-                    sampledOutput[y][x] = temp[i].alt.value
-                    x += 1
-        y += 1
-    for target in CTOI_df.iloc:
-        targetPos = SkyCoord(ra=target['RA'], dec=target['Dec'], distance=target['Stellar Distance (pc)'] ,
-                             unit=(u.hourangle, u.deg, u.pc), obstime=obsDateTime)
-        with erfa_astrom.set(ErfaAstromInterpolator(int(steps/60) * u.hour)): 
-            temp = targetPos.transform_to(aaFrame)
-            if np.array([temp.alt.value>=50]).sum() >= 60 and not(target['TIC ID'] in dontObserve):
-                # Si el target esta sobre 50 grados por 1 hora o mas, se muestrea cada 1 hora.
-                x = 0
-                for i in samplingIndexes:
-                    # Si no esta sobre 50 grados por 1 hora minimo, no se rellena.
-                    sampledOutput[y][x] = temp[i].alt.value
-                    x += 1
-        y += 1
-    print(f'Simulating nights: {np.round((day+1) * 100 /days_range, 2)}%', end="\r", flush=True)
-    pd.DataFrame(sampledOutput).to_csv(f"data{contador}.csv", index=False, header=False)
-    contador += 1
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    for day in range(days_range):
+        if weekday == 0:  # Monday
+            if not(day % 7 == 0 or (day + 5) % 7 == 0):
+                continue  # In this version this statement skips days that are not M,W.
+        elif weekday == 2:  # Wednesday
+            if not(day % 7 == 0 or (day + 2) % 7 == 0):
+                continue  # In this version this statement skips days that are not W,M.
+        times = Time(datetime(int(date[0]), int(date[1]), int(date[2]), 23, 59, 0)) + day*u.day + np.linspace(-6, 9, 901)* u.hour
+        # Ahora se busca el momento donde el sol cruza los -18° de altitud desde la posicion en La Silla
+
+        sunPos = get_sun(times)
+        aaFrame = AltAz(obstime=times, location=laSilla)
+        aa_sunPos = sunPos.transform_to(aaFrame) 
+        begin = np.argmax(aa_sunPos.alt.value < -12)
+        end = np.argmax(np.flip(aa_sunPos.alt).value < -18)
+        twiBegin = times[begin]
+        twiEnd = np.flip(times)[end]
+        # Ahora que tenemos la hora de inicio y de termino del twilight, tenemos el tiempo para calcular efemerides de luna y targets.
+        obsTimeDelta = twiEnd + .5*u.hour - (twiBegin - .5*u.hour)
+        obsTimeDelta = TimeDelta((obsTimeDelta.value * 24 // 1) / 24)
+        steps = int(np.round(obsTimeDelta.sec, 0) / 60)
+        obsDateTime = twiBegin - .5*u.hour + obsTimeDelta * np.linspace(0, 1, steps+1)
+        with open('nightStartTime.txt', 'a') as f:
+            f.write(str(day)+'|'+
+                    str(obsDateTime[0].datetime.year)+'|'+
+                    str(obsDateTime[0].datetime.month)+'|'+
+                    str(obsDateTime[0].datetime.day)+'|'+
+                    str(obsDateTime[0].datetime.hour)+'|'+
+                    str(obsDateTime[0].datetime.minute)+'\n'
+                   )
+        # Marco AltAz de La Silla
+        aaFrame = AltAz(obstime=obsDateTime, location=laSilla)
+        # Efemerides Luna
+        moonPos = get_moon(obsDateTime, laSilla)
+        aa_moonPos = moonPos.transform_to(aaFrame)  # TODO: output the Moon distance in the sky between targets and the moon.
+        # Indices de muestreo para la altura del target cada una hora
+        samplingIndexes = np.linspace(30, steps-30, int(steps/60), dtype=int)
+        sampledOutput   = np.zeros([len(TOI_df)+len(CTOI_df), int(steps/60)])
+        y = 0
+        for target in TOI_df.iloc:
+            targetPos = SkyCoord(ra=target['RA'], dec=target['Dec'], distance=target['Stellar Distance (pc)'] ,
+                                 unit=(u.hourangle, u.deg, u.pc), obstime=obsDateTime)
+            with erfa_astrom.set(ErfaAstromInterpolator(int(steps/60) * u.hour)): 
+                temp = targetPos.transform_to(aaFrame)
+                if np.array([temp.alt.value>=50]).sum() >= 60 and target['SG2'] <= 3 and not(str(target['TIC ID']) in dontObserve):
+                    # Si el target esta sobre 50 grados por 1 hora o mas, se muestrea cada 1 hora.
+                    x = 0
+                    for i in samplingIndexes:
+                        # Si no esta sobre 50 grados por 1 hora minimo, no se rellena.
+                        sampledOutput[y][x] = temp[i].alt.value
+                        x += 1
+            y += 1
+        for target in CTOI_df.iloc:
+            targetPos = SkyCoord(ra=target['RA'], dec=target['Dec'], distance=target['Stellar Distance (pc)'] ,
+                                 unit=(u.hourangle, u.deg, u.pc), obstime=obsDateTime)
+            with erfa_astrom.set(ErfaAstromInterpolator(int(steps/60) * u.hour)): 
+                temp = targetPos.transform_to(aaFrame)
+                if np.array([temp.alt.value>=50]).sum() >= 60 and not(str(target['TIC ID']) in dontObserve):
+                    # Si el target esta sobre 50 grados por 1 hora o mas, se muestrea cada 1 hora.
+                    x = 0
+                    for i in samplingIndexes:
+                        # Si no esta sobre 50 grados por 1 hora minimo, no se rellena.
+                        sampledOutput[y][x] = temp[i].alt.value
+                        x += 1
+            y += 1
+        print(f'Simulating nights: {np.round((day+1) * 100 /days_range, 2)}%', end="\r", flush=True)
+        pd.DataFrame(sampledOutput).to_csv(f"data{contador}.csv", index=False, header=False)
+        contador += 1
 with open('specObservations.txt', 'w') as f:
     pass
 with open('specObservations.txt', 'a') as f:
+    written = []
     for target in TOI_df.iloc:
+        if target['TIC ID'] in written:
+            continue
         if target['Period (days)'] > days_range / 2 + 1 or target['Period (days)'] == 0:
             prio = '0'
         else:
             prio = str(np.exp(-target['Spectroscopy Observations']))
         f.write(str(target['TIC ID']) +'|'+ prio +'|'+ str(target['Period (days)'])+'\n')
+        written.append(target['TIC ID'])
     for target in CTOI_df.iloc:
+        if target['TIC ID'] in written:
+            continue
         if target['Period (days)'] > days_range / 2 + 1 or target['Period (days)'] == 0:
             prio = '0'
         else:
             prio = str(np.exp(-(target['TESS Mag'] - 9)))
         f.write(str(target['TIC ID']) +'|'+ prio +'|'+ str(target['Period (days)'])+'\n')
+        written.append(target['TIC ID'])
 print('Simulation done! Now starting the optimization process...')
